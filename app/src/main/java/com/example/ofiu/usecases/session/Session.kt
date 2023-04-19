66a1b6f5215c47dcf71004391d3c6d2ae4534c26
Juan Pablo Forero <juandafloo55@gmail.com>
2023-04-18 20:13:58 -0500

    Diseñando la pantalla de session

--- app/src/main/java/com/example/ofiu/usecases/session/Session.kt
+++ app/src/main/java/com/example/ofiu/usecases/session/Session.kt
@@ -1,26 +1,36 @@
 package com.example.ofiu.usecases.session
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.material.Text
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 
 @Composable
 fun SessionApp(){
-    Row(
-        modifier = Modifier
-            .fillMaxSize(),
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.Center
+    Entry()
+    }
+
+@Composable
+fun Entry(modifier: Modifier = Modifier){
+    Box(
+        modifier = Modifier.fillMaxSize().background(Color.Yellow).padding(20.dp)
     ) {
-        Text(text = "PANTALLA PRINCIPAL")
+        Box(
+            modifier = Modifier
+                .background(Color.Blue)
+                .fillMaxWidth().height(400.dp)
+                .clip(RoundedCornerShape(20.dp))
+        )
     }
 }
+
 
 @Preview(showSystemUi = true, showBackground = true)
 @Composable
