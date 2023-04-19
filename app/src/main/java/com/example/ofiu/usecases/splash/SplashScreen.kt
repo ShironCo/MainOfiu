f54acaf8b5d3bb4553c5dd82e7226fffba115498
Juan Pablo Forero <juandafloo55@gmail.com>
2023-04-18 19:12:24 -0500

    Bloque de rotacion de pantalla y animacion de splash

--- app/src/main/java/com/example/ofiu/usecases/splash/SplashScreen.kt
+++ app/src/main/java/com/example/ofiu/usecases/splash/SplashScreen.kt
@@ -1,5 +1,7 @@
 package com.example.ofiu.usecases.splash
 
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -7,10 +9,10 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
@@ -20,16 +22,28 @@ import kotlinx.coroutines.delay
 
 @Composable
 fun SplashScreen(navController: NavHostController){
+
+    var starAnimation by remember {
+        mutableStateOf(false)
+    }
+    val alphaAni = animateFloatAsState(
+        targetValue = if(starAnimation) 1f else 0f,
+        animationSpec = tween(
+            durationMillis = 2000
+        )
+    )
+
     LaunchedEffect(key1 = true){
+        starAnimation = true
         delay(5000)
         navController.popBackStack()
         navController.navigate(AppScreens.Session.route)
     }
-    Splash(Modifier.background(MaterialTheme.colors.background))
+    Splash(Modifier.background(MaterialTheme.colors.background), alpha = alphaAni.value)
 }
 
 @Composable
-fun Splash(modifier: Modifier = Modifier){
+fun Splash(modifier: Modifier = Modifier, alpha: Float){
     Row(
         modifier
             .fillMaxSize()
@@ -39,9 +53,9 @@ fun Splash(modifier: Modifier = Modifier){
     ) {
         Image(painter = painterResource(id = R.drawable.splashlogo),
             contentDescription = "Icon Ofiu",
-            Modifier.weight(1F))
+            Modifier.weight(1F).alpha(alpha = alpha))
         Image(painter = painterResource(id = R.drawable.nombresplashlogo),
             contentDescription = "Name Ofiu",
-            Modifier.weight(1F))
+            Modifier.weight(1F).alpha(alpha = alpha))
     }
 }
